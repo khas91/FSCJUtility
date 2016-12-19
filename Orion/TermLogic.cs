46bb76e24fb8720d9a438bead794a1f5b9262243
Stuart Pierson <spierson@fscj.edu>
2016-12-19 17:01:42 -0500

    Attempting to consolidate logic Both CDMR and PLR contain this logic

--- Orion/TermLogic.cs
+++ Orion/TermLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -560,6 +561,162 @@ namespace Academic
             }
 
             return courseHours.OrderByDescending(x => x).Take(n).Sum();
+        }
+        // ********************************************************************************
+        /// <summary>
+        /// This method finds all academic programs which are effective between minTerm and maxTerm, then finds all catalog versions effective during that time
+        /// and fleshes out each to define the catalog requirements for each catalog version.
+        /// </summary>
+        /// <typeparam name="programCode">If specified, function returns only catalog versions of this program code, pass empty string to omit</typeparam>
+        /// <param name="conn">Connection to vulcan. Should be initialized before invocation</param>
+        /// <param name="minTerm">OrionTerm object representing beginning term, see documentation for The TermLogic namespace 
+        /// in this library for documenation regarding this class</param>
+        /// <param name="maxTerm">OrionTerm object representing ending term</param>
+        /// <returns></returns>
+        /// <created>sjp,12/19/2016</created>
+        /// <changed>sjp,12/19/2016</changed>
+        // ********************************************************************************
+        public static List<AcademicProgram> identifyEffectivePrograms(SqlConnection conn, TermLogic.OrionTerm minTerm, TermLogic.OrionTerm maxTerm, String programCode)
+        {
+            List<AcademicProgram> programs = new List<AcademicProgram>();
+            Dictionary<String, AcademicProgram> programDictionary = new Dictionary<string, AcademicProgram>();
+            SqlCommand comm;
+            SqlDataReader reader;
+
+            comm = new SqlCommand("SELECT DISTINCT                                                                                                                         "
+                                            + "    prog.PGM_CD                                                                                                                        "
+                                            + "    ,prog.AWD_TY                                                                                                                       "
+                                            + "    ,prog.EFF_TRM_D                                                                                                                    "
+                                            + "    ,prog.END_TRM                                                                                                                      "
+                                            + "    ,proggroup.PGM_AREA                                                                                                                "
+                                            + "    ,progarea.PGM_AREA_TYPE                                                                                                            "
+                                            + "    ,proggroup.PGM_AREA_GROUP                                                                                                          "
+                                            + "    ,proggroup.PGM_AREA_OPTN_CD                                                                                                        "
+                                            + "    ,proggroup.PGM_AREA_OPTN_OPER                                                                                                      "
+                                            + "    ,groupcourse.PGM_AREA_GROUP_CRS                                                                                                    "
+                                            + "    ,CASE WHEN prog.AWD_TY = 'VC' THEN prog.PGM_TTL_MIN_CNTCT_HRS_REQD ELSE prog.PGM_TTL_CRD_HRS END AS HRS                            "
+                                            + "    ,prog.PGM_TTL_GE_HRS_REQD                                                                                                          "
+                                            + "FROM                                                                                                                                   "
+                                            + "    MIS.dbo.ST_PROGRAMS_A_136 prog                                                                                                     "
+                                            + "    INNER JOIN MIS.dbo.ST_PROGRAMS_A_136 progarea ON progarea.PGM_CD = prog.PGM_CD                                                     "
+                                            + "	                                              AND progarea.EFF_TRM_A = prog.EFF_TRM_D                                                 "
+                                            + "    INNER JOIN MIS.dbo.ST_PROGRAMS_A_136 proggroup ON proggroup.PGM_CD = prog.PGM_CD                                                   "
+                                            + "	                                              AND proggroup.EFF_TRM_G = prog.EFF_TRM_D                                                "
+                                            + "	                                              AND proggroup.PGM_AREA = progarea.PGM_AREA                                              "
+                                            + "    INNER JOIN MIS.dbo.ST_PROGRAMS_A_PGM_AREA_GROUP_CRS_136 groupcourse ON groupcourse.ISN_ST_PROGRAMS_A = proggroup.ISN_ST_PROGRAMS_A "
+                                            + "WHERE                                                                                                                                  "
+                                            + "    prog.EFF_TRM_D <> ''                                                                                                               "
+                                            + "    AND prog.EFF_TRM_D <= '" + maxTerm + "'                                                                                            "
+                                            + "    AND (prog.END_TRM = '' OR prog.END_TRM >= '" + minTerm + "')                                                                       "
+                                            + "    AND prog.AWD_TY NOT IN ('ND','NC','HS')                                                                                            "
+                                            + (programCode == "" ? " " : ("   AND prog.PGM_CD = '" + programCode + "' "))
+                                            + "ORDER BY                                                                                                                               "
+                                            + "    prog.PGM_CD                                                                                                                        "
+                                            + "    ,prog.EFF_TRM_D                                                                                                                    "
+                                            + "    ,proggroup.PGM_AREA                                                                                                                "
+                                            + "    ,proggroup.PGM_AREA_GROUP", conn);
+            reader = comm.ExecuteReader();
+
+            while (reader.Read())
+            {
+                String curProgramCode = reader["PGM_CD"].ToString();
+                String effectiveTerm = reader["EFF_TRM_D"].ToString();
+                String endTerm = reader["END_TRM"].ToString();
+                String courseID = reader["PGM_AREA_GROUP_CRS"].ToString().Trim();
+                int areaNum = int.Parse(reader["PGM_AREA"].ToString());
+                int groupNum = int.Parse(reader["PGM_AREA_GROUP"].ToString());
+
+                AcademicProgram prog;
+
+                if (!programDictionary.ContainsKey(curProgramCode))
+                {
+                    prog = new AcademicProgram();
+                    prog.progCode = curProgramCode;
+                    prog.awardType = reader["AWD_TY"].ToString();
+                    prog.catalogChanges = new List<AcademicProgram.CatalogChange>();
+                    prog.catalogDictionary = new Dictionary<string, AcademicProgram.CatalogChange>();
+                    programs.Add(prog);
+                    programDictionary.Add(curProgramCode, prog);
+                }
+                else
+                {
+                    prog = programDictionary[curProgramCode];
+                }
+
+                AcademicProgram.CatalogChange catalog;
+
+                if (!prog.catalogDictionary.ContainsKey(effectiveTerm))
+                {
+                    catalog = new AcademicProgram.CatalogChange();
+                    catalog.effectiveTerm = effectiveTerm;
+                    catalog.endTerm = endTerm;
+                    catalog.totalProgramHours = (int)float.Parse(reader["HRS"].ToString());
+                    catalog.totalGeneralEducationHours = (int)float.Parse(reader["PGM_TTL_GE_HRS_REQD"].ToString());
+                    catalog.totalCoreAndProfessionalHours = catalog.totalProgramHours - catalog.totalGeneralEducationHours;
+                    catalog.effectiveTermYear = int.Parse(catalog.effectiveTerm.Substring(0, 4));
+                    catalog.effectiveTermTerm = int.Parse("" + catalog.effectiveTerm[4]);
+                    catalog.endTermYear = catalog.endTerm != "" ? int.Parse(catalog.endTerm.Substring(0, 4)) : 9999;
+                    catalog.endTermTerm = catalog.endTerm != "" ? int.Parse("" + catalog.endTerm[4]) : 9;
+                    catalog.areaDictionary = new Dictionary<int, AcademicProgram.CatalogChange.Area>();
+                    catalog.flatCourseArray = new List<string>();
+                    prog.catalogChanges.Add(catalog);
+                    prog.catalogDictionary.Add(effectiveTerm, catalog);
+                    catalog.areas = new List<AcademicProgram.CatalogChange.Area>();
+                }
+                else
+                {
+                    catalog = prog.catalogDictionary[effectiveTerm];
+                }
+
+                AcademicProgram.CatalogChange.Area area;
+
+                if (!catalog.areaDictionary.ContainsKey(areaNum))
+                {
+                    area = new AcademicProgram.CatalogChange.Area();
+                    area.areaNum = areaNum;
+                    area.areaType = reader["PGM_AREA_TYPE"].ToString();
+                    area.groupDictionary = new Dictionary<int, AcademicProgram.CatalogChange.Area.Group>();
+                    area.groups = new List<AcademicProgram.CatalogChange.Area.Group>();
+                    catalog.areas.Add(area);
+                    catalog.areaDictionary.Add(area.areaNum, area);
+                }
+                else
+                {
+                    area = catalog.areaDictionary[areaNum];
+                }
+
+                AcademicProgram.CatalogChange.Area.Group group;
+
+                if (!area.groupDictionary.ContainsKey(groupNum))
+                {
+
+                    group = new AcademicProgram.CatalogChange.Area.Group();
+                    group.groupNum = groupNum;
+                    group.optCode = reader["PGM_AREA_OPTN_CD"].ToString();
+                    group.operatorCode = reader["PGM_AREA_OPTN_OPER"].ToString();
+                    group.courseDictionary = new Dictionary<string, Course>();
+                    group.courses = new List<Course>();
+                    area.groups.Add(group);
+                    area.groupDictionary.Add(groupNum, group);
+                }
+                else
+                {
+                    group = area.groupDictionary[groupNum];
+                }
+
+                Course course;
+                
+                course = new Course();
+                course.courseID = courseID;
+                group.courseDictionary.Add(courseID, course);
+                group.courses.Add(course);
+                catalog.flatCourseArray.Add(courseID);
+
+            }
+
+            reader.Close();
+
+            return programs;
         }
     }
 }
